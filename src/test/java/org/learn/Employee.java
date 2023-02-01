6571413ec57214ef420119bf2338d124904b9704
Sarath13-dev <ramsssjrs@gmail.com>
2023-02-01 10:22:59 +0530

    first

---
+++ src/test/java/org/learn/Employee.java
@@ -0,0 +1,20 @@
+package org.learn;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Employee {
+	
+	@Test
+	private void tc0() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver  driver = new ChromeDriver();
+		Thread.sleep(5000);
+		driver.get("https://www.facebook.com/");
+	}
+	
+	
+	}
